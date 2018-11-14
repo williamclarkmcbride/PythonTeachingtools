da62af2b44c901f9851e8f2e411e707110aad64f
williamclarkmcbride <725167+williamclarkmcbride@users.noreply.github.com>
2018-11-14 16:03:29 -0700

    Create tempconvsimple.py

---
+++ tempconvsimple.py
@@ -0,0 +1,8 @@
+temperature: float
+
+print("Please enter the tempurature in Celcius: ")
+temperature = float(input())
+
+temperature = ((temperature * 9) / 5) + 32
+
+print(f"The temperature in Fahrenheit is {temperature} degrees")
